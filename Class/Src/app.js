d4b5a496fb9fbd75674c01e59d015de67002cbd4
Trenton Smith <98728620+TKS005@users.noreply.github.com>
2022-05-14 22:22:00 -0500

    Add files via upload

---
+++ Class/Src/app.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Breweries from './components/breweries';
+
+function App() {
+  return (
+    <div className="App">
+      <Breweries role="Developer" name="Trenton" />
+    </div>
+  );
+}
+
+export default App;
